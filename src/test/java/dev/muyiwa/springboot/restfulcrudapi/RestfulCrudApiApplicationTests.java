6d56001ff478064dac26b271cc377aa684d398fc
Moyosoreoluwa <dev.muyiwa@gmail.com>
2024-01-20 20:50:45 +0100

    Project Setup.

---
+++ src/test/java/dev/muyiwa/springboot/restfulcrudapi/RestfulCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.muyiwa.springboot.restfulcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulCrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
